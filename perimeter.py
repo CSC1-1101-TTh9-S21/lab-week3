84133460d3c3ea30c7213d9bd41d19382c674925
Emily Prud'hommeaux <prudhome@bc.edu>
2021-02-07 22:49:12 -0500

    Add files via upload

---
+++ perimeter.py
@@ -0,0 +1,18 @@
+# Starter code for week 3 lab, parts 1 and 2
+# perimeter.py
+# Prof. Prud'hommeaux
+
+# Get some input from the user.
+a=int(input("Enter the length of one side of the rectangle: "))
+b=int(input("Enter the length of the other side of the rectangle: "))
+
+# Print out the perimeter of the rectangle
+c = a + a + b + b
+print("The perimeter of your rectangle is: ", c)
+
+# Part 1: Rewrite this code so that it gets the sides from
+# command line arguments (i.e., Run... Customized)
+
+# Part 2: Rewrite this code so that there are two functions:
+# a function that calculates the perimeter
+# a main function
